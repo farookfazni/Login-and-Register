a8d6c16edeed76bd5d72b0db73c835fafc9b361e
farookfazni <faznifarook@gmail.com>
2019-12-26 16:26:13 +0530

    Login and Register

---
+++ dbh.php
@@ -0,0 +1,14 @@
+<?php
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "login";
+
+
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
+
+
+
+/* <?php
+    include_once 'dbh.php';
+?>
